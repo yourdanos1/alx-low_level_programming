a2cd49ecdecd9425011f03ed9a94315131ad316d
yourdanos1 <yourdanosdemelew@gmail.com>
2023-09-18 07:01:12 -0700

    pointersn

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,13 +9,12 @@
 void reset_to_98(int *n) 
 { 
   *n=98;
-  
 }
 int main (void) 
 {
-  int n=402;  
-  printf("n=%d/n", n);
-  reset_to_98(&n);
-  printf("n=%d/n", n);
+  int p=402;  
+  printf("n=%d\n", p);
+  reset_to_98(&p);
+  printf("n=%d\n", p);
   return (0);
 }
