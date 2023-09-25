4b2f489ca48b2556d65d2c41064bbb47a306f051
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-09-25 14:14:58 -0700

    print chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * a function that prints the chessboard.
+ *
+ * returns the printed values
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int row;
+int col;
+for (row = 0; row < 8; row++)
+{
+for (col = 0; col < 8; col++)
+{
+printf("%c ", a[row][col]);
+}
+printf("\n");
+}
+}
