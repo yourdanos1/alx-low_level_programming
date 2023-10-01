638741368ad126405d1298b626c1a32c561c2a02
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-09-30 23:34:49 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,20 +10,14 @@ int factorial(int n)
 {
 if (n < 0)
 {
-return (-1);
+return -1;
 }
 else if (n == 0)
 {
-return (1);
+return 1;
 }
 else
 {
-int result = 1;
-int i;
-for (i = 1; i <= n; i++)
-{
-result *= i;
-}
-return (result);
+return n * factorial(n - 1);
 }
 }
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "mIN.H"
+/**
+ * x raised to the power of y
+ *
+ * returns the value of the function
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return( -1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
