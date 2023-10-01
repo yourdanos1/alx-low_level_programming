064a72c586f91b1b8c5837c40fe601d67a7b1293
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-10-01 00:39:08 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * natural square root of a number
+ *
+ * returns root
+ */
+int _sqrt_recursive_calculation(int n, int guess, int prev)
+{
+if (guess * guess == n)
+return (guess);
+if (guess == prev)
+return (-1);
+if (guess * guess > n)
+return (_sqrt_recursive_calculation(n, (guess + prev) / 2, guess));
+return (_sqrt_recursive_calculation(n, (n / guess + guess) / 2, guess));
+}
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0 || n == 1)
+return (n);
+return (_sqrt_recursive_calculation(n, n / 2, 0));
+}
