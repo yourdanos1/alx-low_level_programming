59830f39de39def8dbbe1c8b4c35b6142d33cd26
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-10-03 09:11:16 -0700

    change

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-/** prints its name
+/**
+ * prints its name
  *
  */
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-if (argc > 0) 
+if (argc > 0)
 {
 printf("%s\n", argv[0]);
 }
-return 0;
+return (0);
 }
