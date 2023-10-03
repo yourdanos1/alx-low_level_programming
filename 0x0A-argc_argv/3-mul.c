e22ab60a12b8ad5758093bb0924cf939a9b60f16
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-10-03 10:31:02 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * multiplies two strings 
+ *
+ */
+
+int stringToInteger(const char *str)
+{
+int result = 0;
+int sign = 1;
+int i =0;
+if (str[0] == '-')
+{
+sign = -1;
+i = 1;
+}
+for (; str[i] != '\0'; ++i)
+{
+if (str[i] >= '0' && str[i] <= '9')
+{
+result = result * 10 + (str[i] - '0');
+}
+else
+{
+return (0);
+}
+}
+return (result * sign);
+}
+int main(int argc, char *argv[])
+{
+int result;
+int num1;
+int num2;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+num1 = stringToInteger(argv[1]);
+num2 = stringToInteger(argv[2]);
+result = num1 * num2;
+printf("%d\n", result);
+return (0);
+}
