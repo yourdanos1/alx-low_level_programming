bf6765a0032cf3f548066e772e35e606bd56ed0a
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-10-09 11:22:00 -0700

    initdog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = malloc(strlen(name) + 1);
+d->owner = malloc(strlen(owner) + 1);
+if (d->name == NULL || d->owner == NULL)
+{
+fprintf(stderr, "Memory allocation error\n");
+exit(1);
+}
+strcpy(d->name, name);
+d->age = age;
+strcpy(d->owner, owner);
+}
