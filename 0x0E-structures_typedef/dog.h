971f7b76c5434bf5b46c73e59a6396828ef3d763
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-10-10 03:43:02 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,10 @@ char *name;
 char *owner;
 float age;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 
 
 #endif
