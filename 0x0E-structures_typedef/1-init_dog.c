ca8e6bd10fca853436b699b42e20232d0e847bf0
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-10-10 11:01:23 -0700

    change

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,21 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <string.h> 
+/*
+ * init_dog - initalize a variable of type struct_dog
+ * @d: pointer
+ * @name: holding chharacter
+ * @age: holding integer
+ * @owner: holding cgaracter
+ *
+ * Return: pointer
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-return;
-snprintf(d->name, (int)sizeof(d->name), "%s", name);
+if (d != NULL)
+{
+d->name = strdup(name);
 d->age = age;
-snprintf(d->owner, (int)sizeof(d->owner), "%s", owner);
+d->owner = strdup(owner);
+}
 }
