e101bd83455af99e1d299072562b4a67813a81a5
Yourdanos1 <yourdanosdemelew@gmail.com>
2023-10-13 08:30:04 -0700

    change

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - function that holds a function
+ * @array: integer
+ * @size: size of array
+ * @action: a function to pointer
+ *
+ * Returm: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
